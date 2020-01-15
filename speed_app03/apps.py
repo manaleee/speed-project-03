89dc124b6951d78982dee734a903463a6d8a2d6b
Manal Alkandari <manal_alkandri@hotmail.com>
2020-01-15 17:25:12 +0300

    speed project 3

---
+++ speed_app03/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpeedApp03Config(AppConfig):
+    name = 'speed_app03'
---
+++ speed_app03/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def speed03_function(request):
+    return render(request,'speed03_page.html')
+
+
+
